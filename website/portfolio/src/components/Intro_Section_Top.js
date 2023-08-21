ea7a5ec06674d59aa32e98979e3075a4c4b90351
Himanshu Chandna <himanshuchandna0007@gmail.com>
2023-08-21 23:11:37 +0530

    Profile Image Added

--- website/portfolio/src/components/Intro_Section_Top.js
+++ website/portfolio/src/components/Intro_Section_Top.js
@@ -4,16 +4,22 @@ export default function Intro_Section_Top() {
   return (
     <div>
 
-        <>
+        
 
-        <div className="profile-intro">    
-            <img className="profile" src="" alt="Profile"></img>
-            <p className="profile-intro">Hi, I'm Himanshu Chandna.</p>
-            <button type="button" class="btn btn-outline-dark">Linkedin</button>
-            <button type="button" class="btn btn-outline-dark">Github</button>
+        <div className="profile-intro">
+            <div class="photo">    
+              <img class="profile" src="https://media.licdn.com/dms/image/C4D03AQHMTXHJVcmdsg/profile-displayphoto-shrink_800_800/0/1623403228589?e=2147483647&v=beta&t=5TaGzZZB7VMsn-wVIo4QyHRJ_iSdv82px4hw-b5AorY" alt="Profile"></img>
+            </div>
+            <div class="profile-desc">
+              <p className="profile-intro">Hi, I'm Himanshu Chandna.</p>
+                <div class="profile_social_btn">
+                  <button type="button" class="btn btn-outline-dark">Linkedin</button>
+                  <button type="button" class="btn btn-outline-dark">Github</button>
+                </div>
+            </div>
         </div>
         
-        </>
+        
 
     </div>
   )
